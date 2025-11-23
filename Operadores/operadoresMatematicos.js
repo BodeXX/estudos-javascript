b9aadb3d20de933728e639e80d7672cfeaa93028
BodeXX <jonasalberto.dev@gmail.com>
2025-11-22 22:59:08 -0300

    feat: Operadores Matemáticos

---
+++ Operadores/operadoresMatematicos.js
@@ -0,0 +1,58 @@
+const numero1 = 4;
+const numero2 = 8;
+
+
+// Operador de soma
+const soma = numero1 + numero2;
+
+console.log(`Soma: ${soma}`);
+
+// Operador de subtração
+const subtracao = numero1 - numero2;
+
+console.log(`Subtração: ${subtracao}`);
+
+// Operador de divisão
+const divisao = numero1 / numero2;
+
+console.log(`Divisão: ${divisao}`);
+
+// Operador de multiplicação
+const multiplicacao = numero1 * numero2;
+
+console.log(`Multiplicação: ${multiplicacao}`);
+
+// Ordem de precedencia
+const contaComplexa = (numero1 + numero2) * numero1;
+
+console.log(`Conta Complexa: ${contaComplexa}`);
+
+// Resto de divisão
+const resto = 12 % 2;
+
+console.log(`Resto da divisão: ${resto}`);
+
+// Potência
+const potencia = 2 ** 3;
+
+console.log(`Potência: ${potencia}`);
+
+// Incremento
+let contador = 5;
+
+contador++;
+console.log(`Incremento: ${contador}`);
+
+// Decremento
+contador--;
+console.log(`Decremento: ${contador}`);
+
+// Operadores por atribuição
+let numero3 = 2;
+
+numero3 += 3;
+// numero3 -= 3;
+// numero3 *= 3;
+// numero3 /= 3;
+
+console.log(numero3);
