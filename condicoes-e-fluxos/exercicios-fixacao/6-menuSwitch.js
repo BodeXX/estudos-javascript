51a35722fc0b0b173c0d150436018b691d67ac38
BodeXX <jonasalberto.dev@gmail.com>
2025-11-30 01:05:22 -0300

    feat: finaliza exercicio 6 - logica de menu com switch

---
+++ condicoes-e-fluxos/exercicios-fixacao/6-menuSwitch.js
@@ -0,0 +1,17 @@
+// Lógica de navegação do menu principal
+
+let opcao = 1
+
+switch (opcao) {
+    case 1:
+        console.log('Cadastrar');
+        break;
+    case 2:
+        console.log('Listar');
+        break;
+    case 3:
+        console.log('Sair');
+        break;
+    default:
+        console.log('Opção inválida');
+}
