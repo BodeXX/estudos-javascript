86951932a0539de98b5fb382fab004526ab42c76
BodeXX <jonasalberto.dev@gmail.com>
2025-12-03 21:25:37 -0300

    feat: finaliza exercicio 4 -Executa contagem regressiva de 10 a 0

---
+++ lacos-operadores-decisao/Desafios/4-contagemRegressiva.js
@@ -0,0 +1,10 @@
+// Executa uma contagem regressiva de 10 a 0
+let contador = 10;
+
+// Enquanto o contador for MAIOR ou IGUAL a 0
+while (contador >= 0) {
+    console.log(`Número ${contador}`);
+    contador--;
+}
+
+console.log('Contagem finalizada!');
