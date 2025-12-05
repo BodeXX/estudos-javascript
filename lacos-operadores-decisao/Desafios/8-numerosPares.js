d444e9d767442a198ffd04286226609afd7084e6
BodeXX <jonasalberto.dev@gmail.com>
2025-12-05 20:44:21 -0300

    feat: finaliza exercicio 8 -Implementa logica de exibicao de numeros pares

---
+++ lacos-operadores-decisao/Desafios/8-numerosPares.js
@@ -0,0 +1,8 @@
+// Percorre os números de 1 a 50
+for (let i = 1; i <= 50; i++) {
+
+    // Verifica se o número é par (resto da divisão por 2 é zero)
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
