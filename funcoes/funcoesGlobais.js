f906e53ed55725b600c4fb379ddee6d87eba90a4
BodeXX <jonasalberto.dev@gmail.com>
2025-12-06 16:46:20 -0300

    feat: implementa contador com setInterval e clearInterval

---
+++ funcoes/funcoesGlobais.js
@@ -0,0 +1,15 @@
+let saudacao = () => {
+    console.log('Iae, beleza?');
+}
+
+//setTimeout(saudacao, 2000) // ms
+
+let contador = 0;
+
+const id = setInterval(() => {
+    contador++
+    console.log(`Tempo decorrido (em segundos): ${contador}`);
+    if (contador == 10){
+    clearInterval(id)
+    }
+}, 1000);
