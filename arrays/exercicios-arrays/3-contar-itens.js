8a9d183c04e11f3a66c5d52f34372ec89d1cf02a
BodeXX <jonasalberto.dev@gmail.com>
2025-12-07 23:30:04 -0300

    feat(arrays): Finaliza exercicio 3 - Exibe quantidade de itens com length

---
+++ arrays/exercicios-arrays/3-contar-itens.js
@@ -0,0 +1,6 @@
+// Crie um array com nomes de cidades e exiba no console a quantidade total de itens usando a propriedade .length.
+
+const cidades = ['são sebastião do passé', 'salvador', 'candeias', 'camaçari'];
+
+// A propriedade length lê a lista e passa a quantidade total de itens
+console.log(`A quantidade de cidades na lista é de: ${cidades.length}`);
