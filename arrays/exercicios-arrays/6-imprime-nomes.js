cd175cecd3657b2d9d54b3a5b5366d5b580fc77d
BodeXX <jonasalberto.dev@gmail.com>
2025-12-08 20:51:57 -0300

    feat(arrays): Finaliza exercicio 6 - Implementa loop forEach para saudacao

---
+++ arrays/exercicios-arrays/6-imprime-nomes.js
@@ -0,0 +1,8 @@
+// Crie um array com nomes de usuários. Use forEach para imprimir "Olá, [nome]!" para cada um.
+
+const nomes = ['user1', 'user2', 'user3', 'user4', 'user5'];
+
+// Itera sobre a lista de usuários imprimindo uma saudação para cada um
+nomes.forEach((nome) => {
+    console.log(`Olá, ${nome}!`);
+})
