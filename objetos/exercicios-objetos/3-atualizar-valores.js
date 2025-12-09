eaba13e96c7abbaeada3a4ac0d096456f404199f
BodeXX <jonasalberto.dev@gmail.com>
2025-12-09 17:26:29 -0300

    feat(objetos): finaliza exercicio 3 - atualizacao de valor de propriedade

---
+++ objetos/exercicios-objetos/3-atualizar-valores.js
@@ -0,0 +1,15 @@
+// Modifique a propriedade "idade" com um novo valor.
+
+const pessoa = {
+    nome: 'Jonas',
+    idade: 27,
+    profissao: 'Programador'
+}
+
+// antes de modificar a propriedade
+console.log(pessoa.idade);
+
+pessoa.idade = 28;
+
+// depois da modificação
+console.log(pessoa.idade);
