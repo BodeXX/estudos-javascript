fd2c33a9e0fd246ed9ef54b21b2705bf9094b086
BodeXX <jonasalberto.dev@gmail.com>
2025-12-09 17:32:57 -0300

    feat(objetos): finaliza exercicio 4 - adicao dinamica de propriedade

---
+++ objetos/exercicios-objetos/4-adicionar-propriedade.js
@@ -0,0 +1,14 @@
+// Adicione ao objeto uma nova propriedade chamada "cidade".
+
+const pessoa = {
+    nome: 'Jonas',
+    idade: 27,
+    profissao: 'Programador'
+}
+
+console.log('Objeto antes: ', pessoa);
+
+// Implementa nova propriedade ao objeto 'pessoa'
+pessoa.cidade = 'Salvador';
+
+console.log('Objeto depois: ', pessoa);
