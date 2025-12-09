49ff834f46c9f57e7161c86b327241ceafe6cba6
BodeXX <jonasalberto.dev@gmail.com>
2025-12-09 18:24:56 -0300

    feat(objetos): finaliza exercicio 6 - array de objetos

---
+++ objetos/exercicios-objetos/6-lista-pessoas.js
@@ -0,0 +1,9 @@
+// Crie um array com 3 objetos, cada um representando uma pessoa com nome e idade.
+
+const listaPessoas = [
+    { nome: 'Jonas', idade: 27},
+    { nome: 'Alberto', idade: 29},
+    { nome: 'Ana', idade: 25 }
+]
+
+console.log(listaPessoas);
