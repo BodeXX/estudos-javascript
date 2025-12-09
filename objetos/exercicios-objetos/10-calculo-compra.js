74b278d3665c5b5243b4d26da871c8807250cf3d
BodeXX <jonasalberto.dev@gmail.com>
2025-12-09 20:18:06 -0300

    feat(objetos): finaliza exercicio 10 - calculo simples com propriedades de objeto

---
+++ objetos/exercicios-objetos/10-calculo-compra.js
@@ -0,0 +1,11 @@
+// Crie um objeto produto com preco e quantidade, e calcule o valor total da compra(preco * quantidade).
+
+const carro = {
+    nome: 'onix',
+    preco: 100,
+    quantidade: 3
+}
+
+console.log('O nome do produto é: ', carro.nome);
+
+console.log('Valor total: ', carro.preco * carro.quantidade);
