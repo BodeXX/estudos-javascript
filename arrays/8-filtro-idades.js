3b60269c04fab1f15c319280d24fb17cc4c14811
BodeXX <jonasalberto.dev@gmail.com>
2025-12-08 23:41:49 -0300

    feat(arrays): Finaliza exercicio 8 - Filtra idades maiores ou iguais a 18

---
+++ arrays/8-filtro-idades.js
@@ -0,0 +1,7 @@
+// Crie um array com idades. Use filter para retornar apenas as idades maiores ou iguais a 18 e exiba o resultado.
+
+const idades = [27, 18, 17, 30, 15, 16];
+
+const maiorIdade = idades.filter(idade => idade >= 18);
+
+console.log(maiorIdade);
