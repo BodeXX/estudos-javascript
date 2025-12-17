2ee8d609da9db6e7b491438b3b672d8f63678c49
BodeXX <jonasalberto.dev@gmail.com>
2025-12-16 22:22:34 -0300

    feat(arrays): implementa destructuring posicional em lista de linguagens

---
+++ fundamentos-modernos/exercicios-fundamentos-modernos/2-destructuring-arrays.js
@@ -0,0 +1,7 @@
+// Crie um array com 3 linguagens de programação. Use destructuring para criar variáveis ling1, ling2 e ling3.
+
+let linguagens = ['Python', 'Golang', 'TypeScript'];
+
+const [ling1, ling2, ling3] = linguagens;
+
+console.log(`> Linguagem 1: ${ling1}\n> Linguagem 2: ${ling2}\n> Linguagem 3: ${ling3}`);
