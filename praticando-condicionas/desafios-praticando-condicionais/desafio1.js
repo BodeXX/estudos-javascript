88adc4768ed2bd145f99eb7a456af454b77fe11c
BodeXX <jonasalberto.dev@gmail.com>
2025-12-19 23:57:09 -0300

    feat(praticando-condicionais): Finaliza Desafio 1 Controle de acesso

---
+++ praticando-condicionas/desafios-praticando-condicionais/desafio1.js
@@ -0,0 +1,9 @@
+// Desafio: Controle de Acesso
+// Crie um programa que verifique se a pessoa tem 18 anos ou mais.
+// Se tiver, exiba "Acesso permitido". Caso contrário, exiba "Acesso negado".
+
+const idade = 20; // Altere este valor para testar
+
+const verificaIdade = idade >= 18 ? 'Acesso permitido' :  'Acesso negado';
+
+console.log(verificaIdade);
