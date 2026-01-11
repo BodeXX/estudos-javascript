7b9fac3b2b1bb09c29d9cd9610a86216ad8cf5cd
BodeXX <jonasalberto.dev@gmail.com>
2026-01-11 20:11:57 -0300

    feat(praticando-funcoes): Desafio 3 Calculo de media escolar

---
+++ praticando-funcoes/desafios-praticando-funcoes/desafio3.js
@@ -0,0 +1,9 @@
+// Desafio: Calculando a média
+// Recebe duas notas e retorna a média aritmética
+
+const calcularMedia = (nota1, nota2) => {
+    return (nota1 + nota2) / 2;
+}
+
+console.log(calcularMedia(7, 9));
+
