775dc81989fd96add4386fcfbc3caa9d999828d5
BodeXX <jonasalberto.dev@gmail.com>
2026-01-26 21:20:46 -0300

    feat(praticando-funcoes): Desafio 10 Sistema de sorteio com delay e avaliacao

---
+++ praticando-funcoes/desafios-praticando-funcoes/desafio10.js
@@ -0,0 +1,55 @@
+// Desafio: Sistema de sorteio com avaliação de participantes
+
+/*
+Deve sortear aleatoriamente um nome da lista de participantes.
+Deve Exibir esse nome com um pequeno atraso (como se fosse um suspense).
+Deve avaliar se o participante foi premiado ou não com base na pontuação:
+
+    - Acima de 80: "Parabéns, você foi premiado!"
+    - Entre 50 e 80: "Você quase conseguiu! Fique de olho nos próximos sorteios."
+    - Abaixo de 50: "Infelizmente, não foi dessa vez."
+
+Desenvolver função para sortear um nome aleatoriamente.
+Desenvolver função para exibir o nome sorteado com 2 segundos de atraso
+Desenvolver função para avaliar a pontuação.
+Desenvolver função final que organize o fluxo completo do sorteio.
+*/
+
+const sortearParticipante = (listaNome) => {
+    const indice = Math.floor(Math.random() * listaNome.length);
+    return listaNome[indice];
+};
+
+const avaliarPontuacao = (pontuacao) => {
+    if (pontuacao > 80) {
+        return "Parabéns, você foi premiado!";
+    } else if (pontuacao >= 50) {
+        return "Você quase conseguiu! Fique de olho nos próximos sorteios.";
+    } else {
+        return "Infelizmente, não foi dessa vez.";
+    }
+};
+
+
+const exibirResultado = (participante) => {
+    console.log("Sorteando...");
+    setTimeout(() => {
+        console.log(`Participante sorteado: ${participante.nome}`);
+        console.log(`Pontuação: ${participante.pontuacao}`);
+        console.log(avaliarPontuacao(participante.pontuacao));
+    }, 2000);
+};
+
+
+function realizarSorteio(lista) {
+    const sorteado = sortearParticipante(lista);
+    exibirResultado(sorteado);
+}
+
+const participantes = [
+    { nome: "Laura", pontuacao: 92 },
+    { nome: "Pedro", pontuacao: 67 },
+    { nome: "Clara", pontuacao: 44 }
+];
+
+realizarSorteio(participantes);
